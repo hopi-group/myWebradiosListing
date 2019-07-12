e002770c1a2a2664a9c61f0db76ecb6ebc912e0e
hopi96 <52815739+hopi96@users.noreply.github.com>
2019-07-12 13:57:34 +0200

    Update webradios.ts

--- src/app/webradios.ts
+++ src/app/webradios.ts
@@ -2,16 +2,19 @@ export const webradios = [
   {
     name: 'IZI WEBRADIO',
     description: 'webradio of summer',
-    link: 'http://listen.radionomy.com/alfunkwebradio'
+    link: 'http://listen.radionomy.com/alfunkwebradio',
+    image: 'https://i3.radionomy.com/radios/400/94cede45-3196-4849-8c4f-5ab27f07ef46.png'
   },
   {
     name: 'GEEKS WEBRADIO',
     description: 'webradio of summer',
-    link: 'http://listen.radionomy.com/bluescafewebradio'
+    link: 'http://listen.radionomy.com/bluescafewebradio',
+    image: 'https://ws.shoutcast.com/images/contacts/a/a765/a765551b-3a82-485d-9c63-bf7dc3e335bb/radios/8A0ED71A-BB89-481B-A62E-32F6324E7103/8A0ED71A-BB89-481B-A62E-32F6324E7103.png'
   },
   {
     name: 'Free',
     description: 'webradio of summer',
-    link: 'http://listen.radionomy.com/togogeeks'
+    link: 'http://listen.radionomy.com/togogeeks',
+    image: 'https://i3.radionomy.com/radios/400/0be77c4c-1f16-4eb2-8b8e-bef9be469c22.jpg'
   }
 ];
